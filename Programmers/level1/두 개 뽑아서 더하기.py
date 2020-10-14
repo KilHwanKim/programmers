e2b99ea4d01630ca31bea042555a1eabc7b5bbb1
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2020-10-14 17:44:00 +0900

    Update 두 개 뽑아서 더하기.py

--- Programmers/level1/두 개 뽑아서 더하기.py
+++ Programmers/level1/두 개 뽑아서 더하기.py
@@ -1,1 +1,11 @@
-
+def solution(numbers):
+    answer = []
+    result1 = numbers.copy()
+    while result1:
+        M = result1.pop(0)
+        result2 = result1.copy()
+        while result2:
+            N = result2.pop(0)
+            answer.append(M+N)
+    
+    return sorted(list(set(answer)))
