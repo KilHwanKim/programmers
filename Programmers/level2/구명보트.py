a5cd4a85ecfa72484051f8ae4fd9ad22004e6261
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2020-12-11 19:22:26 +0900

    Update 구명보트.py
    완료

--- Programmers/level2/구명보트.py
+++ Programmers/level2/구명보트.py
@@ -1,13 +1,13 @@
 def solution(people, limit):
-    answer = 0
-    people.sort(reverse=True)
-    ship=0
-    while people:
-        ship=people[0]
-        del people[0]
-        for index in range(len(people)):
-            if people[index]+ship<=limit:
-                del people[0]
-                break  
+    people.sort()
+    heavy = len(people)-1
+    light=0
+    answer =0
+    while light<=heavy:
+        if people[heavy]+people[light]<=limit:
+            heavy -=1
+            light+=1
+        else:
+            heavy-=1
         answer+=1
     return answer
