ab4e51de8cdd0730e9a7bf07af78b303cb71fb30
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-01-14 22:51:11 +0900

    fail.행렬의곱셈.py

---
+++ Programmers/level2/행렬의곱셈.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    answer = []
+    for y in range(len(arr1)):
+        A=[]
+        for x in range(len(arr2)):
+            S=0
+            for z in range(len(arr1[0])):
+                S+=arr1[y][z]*arr2[z][x]
+            A.append(S)
+        answer.append(A)
+    return answer
