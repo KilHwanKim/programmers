72779e8c961ad59b045cf3f6d64ac49f33abe9b3
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-01-15 23:42:27 +0900

    Update 행렬의곱셈.py

--- Programmers/level2/행렬의곱셈.py
+++ Programmers/level2/행렬의곱셈.py
@@ -2,9 +2,9 @@ def solution(arr1, arr2):
     answer = []
     for y in range(len(arr1)):
         A=[]
-        for x in range(len(arr2)):
+        for x in range(len(arr2[0])):
             S=0
-            for z in range(len(arr1[0])):
+            for z in range(len(arr1[y])):
                 S+=arr1[y][z]*arr2[z][x]
             A.append(S)
         answer.append(A)
