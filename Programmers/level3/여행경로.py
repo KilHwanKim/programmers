fd28d17c3404a8beb3087d09b617721368398789
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-01-28 22:01:34 +0900

    Create 여행경로.py

---
+++ Programmers/level3/여행경로.py
@@ -0,0 +1,14 @@
+def solution(tickets):
+    FT = [False] * len(tickets)
+    answer = ["ICN"]
+    Q=1
+    while Q :
+        Q=[]
+        for index, val in enumerate(tickets):
+            if (not FT[index])  and val[0]==answer[-1]:
+                Q.append([index,val[1]])
+        if Q==[]:
+            return answer
+        Q.sort(key = lambda x : x[-1] )
+        FT[Q[0][0]] =True
+        answer.append(Q[0][-1])
