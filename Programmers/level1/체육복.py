5862ba4d9786a9620951ea73be97f622288b9d69
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-02-04 22:51:07 +0900

    Create 체육복.py

---
+++ Programmers/level1/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    new_lost = set(lost) - set(reserve)
+    new_reserve = set(reserve) - set(lost)
+    for i in new_lost:
+        if i + 1 in new_reserve:
+            new_reserve.remove(i + 1)
+        elif i - 1 in new_reserve:
+            new_reserve.remove(i - 1)
+        else:
+            n-=1
+    return n
