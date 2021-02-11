652db4877c63d9df529eeec0ff5e96946f526e96
KilHwanKim <72796198+KilHwanKim@users.noreply.github.com>
2021-02-12 01:59:42 +0900

    Update 큰수만들기.py

--- Programmers/level2/큰수만들기.py
+++ Programmers/level2/큰수만들기.py
@@ -1,12 +1,9 @@
 def solution(number, k):
-    answer = number
-    while True:
-        if k==0:
-            return answer ## k번 하면 정지
-        sample = answer[1::]
-        for x in range(1,len(str(answer))):
-            if int(sample)< int(answer[0:x]+answer[x+1:]): # 제일 큰 수 구하기 
-                sample = answer[0:x]+answer[x+1:] # 그 숫자를 제외한 수 
-        answer = sample
-        k-=1    
-    return answer
+    stack=[number[0]]
+    for i in list(number)[1::]:
+        while stack and stack[-1]<i and k>0:
+            stack.pop()
+            k-=1
+        else:
+            stack.append(i)
+    return "".join(stack[:len(number)-k])
